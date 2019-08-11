9355a3527b4456559d49ec5f4343191f050f26fd
FelixSundqvist <felix.sundqvist@outlook.com>
2019-08-11 17:08:14 +0300

    edit port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 dotenv.config({path: './config.env'});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 const DB = process.env.DB_CONNECTION.replace(/<password>/, process.env.DB_PASSWORD);
 
